b15cb40580012a1837d267af4a7a63a4990c2b24
PauulM <pawmat7@gmail.com>
2018-11-16 18:58:24 +0100

    Create sign in page

---
+++ src/main/java/datastreaming/webclient/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package datastreaming.webclient.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String welcomePage(){
+        return "welcome";
+    }
+}
